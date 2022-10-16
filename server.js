ae7fbab71ea0b2c85c2d1773b48c5013da05d09a
shubhangNarain <shubhang.20scse1180003@galgotiasuniversity.edu.in>
2022-10-16 18:36:52 +0530

    Done with the Finishing touches

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 const app = express();
 
-app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find();
+    res.render('index', { shortUrls: shortUrls });
+})
+
+app.post('/shortUrls', async (req,res)=>{
+    await ShortUrl.create({full: req.body.fullUrl})
+    res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req,res) => {
+    const shortUrl =  ShortUrl.findOne({ short: req.params.shortUrl}) 
+    if(shortUrl == null) return res.sendStatus(404)
+
+    shortUrl.clicks++
+    shortUrl.save()
+
+    res.redirect(shortUrl.full)
 })
 
 app.listen(process.env.PORT || 4000);
